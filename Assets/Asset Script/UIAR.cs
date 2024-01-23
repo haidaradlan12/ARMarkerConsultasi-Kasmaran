fc0af3e298751587a03e036e64a21188e3920daa
Haidaradlan12 <adlannufahaidar12@gmail.com>
2024-01-23 21:48:55 +0700

    Initial commit

---
+++ Assets/Asset Script/UIAR.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIAR : MonoBehaviour
+{
+    public RectTransform Panel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panel.DOAnchorPos(new Vector2(0, -2500), 1.0f);
+    }
+    public void Kembalibutton()
+    {
+        Panel.DOAnchorPos(new Vector2(0, -2500), 1.0f);
+    }
+    public void Panggil()
+    {
+        Panel.DOAnchorPos(new Vector2(0, 0), 1.0f);
+    }
+}
